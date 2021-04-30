705e9332da7fc503ed331904745ec971720caf16
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2021-04-30 08:24:16 +0200

    🗃️ Added some columns into user table

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -4,15 +4,30 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class User {
   @PrimaryGeneratedColumn('uuid') id: string;
 
-  @Column()
+  @Column({ nullable: false })
   firstName: string;
 
-  @Column()
+  @Column({ nullable: false })
   lastName: string;
 
-  @Column()
+  @Column({ nullable: false, unique: true })
   email: string;
 
-  @Column()
+  @Column({ nullable: false })
   password: string;
+
+  @Column({ type: 'date', nullable: true })
+  birthday: Date;
+
+  @Column({ type: 'boolean', default: false })
+  isAdmin: boolean;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @Column({ nullable: true })
+  jobTitle: string;
+
+  @Column({ type: 'timestamp' })
+  createdAt: Date;
 }
