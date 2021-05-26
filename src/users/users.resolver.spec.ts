ef6d426db0688cd7f860d47fe2772dea7d4d4604
Quentin LAURENT <ql2697@gmail.com>
2021-05-26 20:20:50 +0200

    :green_heart: Trying to add more tests for user

--- src/users/users.resolver.spec.ts
+++ src/users/users.resolver.spec.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnection } from 'typeorm';
 import { TypeOrmConfigService } from '../config.service';
 import { ConfigurationModule } from '../configuration/configuration.module';
+import { CreateUserDto } from './dto/createUser.dto';
 import { User } from './user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
@@ -32,5 +33,18 @@ describe('UsersResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('Should not create user', async () => {
+    const user: CreateUserDto = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@mail.com',
+      password: 'passw0rd',
+    };
+
+    const createUserResponse = await resolver.createUser(user);
+
+    console.log(createUserResponse);
   });
 });
