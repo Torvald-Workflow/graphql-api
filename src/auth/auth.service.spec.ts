71dbb165b4e06c6a7bf71ca125e50515b087980c
Quentin LAURENT <ql2697@gmail.com>
2021-06-04 18:57:45 +0200

    :green_heart: Added test for authentication

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,8 +4,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnection } from 'typeorm';
 import { TypeOrmConfigService } from '../config.service';
+import { CreateUserDto } from '../users/dto/createUser.dto';
 import { UserEntity } from '../users/user.entity';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
@@ -13,8 +15,9 @@ import { JwtStrategy } from './jwt.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userService: UsersService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService, AuthResolver, JwtStrategy],
       imports: [
@@ -34,6 +37,7 @@ describe('AuthService', () => {
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    userService = module.get<UsersService>(UsersService);
 
     await getConnection().dropDatabase();
     await getConnection().runMigrations();
@@ -41,5 +45,75 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Should create a default admin user in database', async () => {
+    const email = 'admin@test.fr';
+    const firstName = 'Admin';
+    const lastName = 'Admin';
+    const password = 'adminpassword';
+
+    const createUserParameters: CreateUserDto = {
+      email,
+      firstName,
+      lastName,
+      password,
+    };
+
+    const user: UserEntity = await userService.createDefaultAdminUser(
+      createUserParameters,
+    );
+
+    expect(user).toMatchObject({
+      email,
+      firstName,
+      lastName,
+      isAdmin: true,
+      isActive: true,
+    });
+  });
+
+  it('Should not validate another user', async () => {
+    const email = 'john.doe@test.fr';
+    const password = 'adminpassword';
+
+    const validatedUser = await service.validateUser(email, password);
+
+    expect(validatedUser).toBeNull();
+  });
+
+  it('Should not validate admin user with wrong password', async () => {
+    const email = 'admin@test.fr';
+    const password = 'adminpasswords';
+
+    const validatedUser = await service.validateUser(email, password);
+
+    expect(validatedUser).toBeNull();
+  });
+
+  it('Should not validate admin user', async () => {
+    const email = 'admin@test.fr';
+    const firstName = 'Admin';
+    const lastName = 'Admin';
+    const password = 'adminpassword';
+
+    const validatedUser = await service.validateUser(email, password);
+
+    expect(validatedUser).toMatchObject({
+      email,
+      firstName,
+      lastName,
+      isAdmin: true,
+    });
+  });
+
+  it('Should login specified user', async () => {
+    const email = 'admin@test.fr';
+
+    const user = await userService.findOne(email);
+
+    const login = await service.login(user);
+
+    expect(login).toHaveProperty('access_token');
   });
 });
